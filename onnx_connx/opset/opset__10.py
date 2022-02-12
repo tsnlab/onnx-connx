276518bcc26ba1f91775ffc6410023ac08e36e76
Semih Kim <semih.kim@gmail.com>
2022-02-12 23:27:22 +0900

    bin/test pytest arguments is added
    * bin/test [onnx version] [[pytest args]] can be used
    * opset__10 and __11 bug is fixed

--- onnx_connx/opset/opset__10.py
+++ onnx_connx/opset/opset__10.py
@@ -3,10 +3,5 @@ from .opset import _float, _int, _string
 
 attrset = {
     'Slice': [],
-    'Resize': [_string('coordinate_transformation_mode', 'half_pixel'),
-               _float('cubic_coeff_a', -0.75),
-               _int('exclude_outside', 0),
-               _float('extrapolation_value', 0.0),
-               _string('mode', 'nearest'),
-               _string('nearest_mode', 'round_prefer_floor')],
+    'Resize': [_string('mode', 'nearest')],
 }
--- onnx_connx/opset/opset__11.py
+++ onnx_connx/opset/opset__11.py
@@ -1,8 +1,14 @@
-from .opset import _int, _ints, _string
+from .opset import _float, _int, _ints, _string
 
 
 attrset = {
     'Clip': [],
     'Conv': [_string('auto_pad', 'NOTSET'), _ints('dilations', []), _int('group', 1),
              _ints('kernel_shape', []), _ints('pads', []), _ints('strides', [])],
+    'Resize': [_string('coordinate_transformation_mode', 'half_pixel'),
+               _float('cubic_coeff_a', -0.75),
+               _int('exclude_outside', 0),
+               _float('extrapolation_value', 0.0),
+               _string('mode', 'nearest'),
+               _string('nearest_mode', 'round_prefer_floor')],
 }
