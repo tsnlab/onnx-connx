7787896ab3e6c982109d50d9511339d9b77b2b33
Semih Kim <semih.kim@gmail.com>
2022-02-16 19:23:56 +0900

    Exclude unsupported features and malformed test cases from test cases

--- onnx_connx/test_connx_backend.py
+++ onnx_connx/test_connx_backend.py
@@ -1,5 +1,6 @@
 import unittest
 
+import onnx
 import onnx.backend.test
 
 from .backend import Backend
@@ -25,12 +26,18 @@ backend_test.exclude(r'test_cast.*BFLOAT16.*')
 backend_test.exclude(r'test_cast.*STRING.*')
 backend_test.exclude(r'test_resize_downsample_sizes_nearest_tf_half_pixel_for_nn')
 
+# Exclude not supported features Resize TF_CROP_AND_RESIZE
+backend_test.exclude(r'test_resize_tf_crop_and_resize')  # We support above Clip-6
+
 # Exclude deprecated operators
 backend_test.exclude(r'test_scatter_*')
 backend_test.exclude(r'test_upsample_*')
 
-# Exclude legacy onnx api
-backend_test.exclude(r'test_operator_clip')  # We support above Clip-6
+# Exclude malformed test cases
+if onnx.__version__ == '1.6.0':
+    backend_test.exclude(r'test_resize_upsample_sizes_nearest_ceil_half_pixel')  # nearest_mode=ceil is missing
+    backend_test.exclude(r'test_resize_upsample_sizes_nearest_floor_align_corners')  # nearest_mode=floor is missing
+    backend_test.exclude(r'test_resize_upsample_sizes_nearest_round_prefer_ceil_asymmetric')  # nearest_mode=round_prefer_ceil is missing
 
 # import all test cases at global scope to make them visible to python.unittest
 globals().update(backend_test.enable_report().test_cases)
